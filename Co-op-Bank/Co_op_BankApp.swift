444188c03fd9601faa0bdaaefdee71ca122af0f3
martin-ngigi <martinwainaina001@gmail.com>
2024-05-15 10:18:55 +0300

    Initial commit

---
+++ Co-op-Bank/Co_op_BankApp.swift
@@ -0,0 +1,17 @@
+//
+//  Co_op_BankApp.swift
+//  Co-op-Bank
+//
+//  Created by Martin Wainaina on 15/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct Co_op_BankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
